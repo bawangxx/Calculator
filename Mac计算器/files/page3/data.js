eaaeb2e8b623b0462d4cb5996914b310090bc60f
小志 <bawangxx@qq.com>
2016-03-02 18:22:57 +0800

    Axure高仿Mac原生计算器

---
+++ Mac计算器/files/page3/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
+var b="url",c="page3.html",d="generationDate",e=new Date(1456913060345.63),f="isCanvasEnabled",g=false,h="variables",i="number1",j="number2",k="sumNum",l="expresion",m="clickFlag",n="flag2",o="page",p="packageId",q="2c57003d7bea4ad5a48e6613d3f1240c",r="type",s="Axure:Page",t="name",u="page3",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFFFFFFF,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
+return _creator();
+})());
---
+++ README.md
@@ -0,0 +1,6 @@
+#Calculator
+
+
+效果图：
+
+![image](http://git.oschina.net/bawangxx/Calculator/raw/master/image/1.gif)
